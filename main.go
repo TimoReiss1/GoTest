72dc0d544b4e33b28939a102215552deb13e170e
TimoReiss <t.reiss@eos-ts.com>
2023-02-06 15:24:19 +0100

    Init full project
    added new files

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"log"
+	"main/handlers"
+	"net/http"
+	"os"
+)
+
+func main() {
+	// Hello world, the web server
+	/*
+		http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+			log.Println("Hello World!")
+			d, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				http.Error(rw, "Oops", http.StatusBadRequest)
+				return
+			}
+			fmt.Fprintf(rw, "Hello %s\n", d)
+		})
+
+		helloHandler := func(w http.ResponseWriter, req *http.Request) {
+			io.WriteString(w, "Hello, world!\n")
+		}
+		http.HandleFunc("/hello", helloHandler)
+
+		goodbyeHandler := func(w http.ResponseWriter, req *http.Request) {
+			io.WriteString(w, "Bye World!\n")
+		}
+		http.HandleFunc("/bye", goodbyeHandler)
+	*/
+
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handlers.NewHello(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/test", hh)
+
+	log.Println("Listing for requests at http://localhost:9000/hello")
+	log.Fatal(http.ListenAndServe(":9000", nil))
+}
