fc221d0c898afbf92d46da185d12c8555fff5861
TimoReiss <t.reiss@eos-ts.com>
2023-02-08 17:34:50 +0100

    Added a method to update existing drinks

--- data/products.go
+++ data/products.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
@@ -15,6 +16,40 @@ type Product struct {
 	CreatedOn   string  `json:"-"`
 	UpdatedOn   string  `json:"-"`
 	DeletedOn   string  `json:"-"`
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextID()
+	productList = append(productList, p)
+}
+
+func UpdateProduct(id int, p *Product) error {
+	p, pos, err := findProductById(id)
+	if err != nil {
+		return err
+	}
+
+	p.ID = id
+	productList[pos] = p
+
+	return nil
+}
+
+var ErrorProductNotFound = fmt.Errorf("Product not found")
+
+func findProductById(id int) (*Product, int, error) {
+	for i, p := range productList {
+		if p.ID == id {
+			return p, i, nil
+		}
+	}
+
+	return nil, -1, ErrorProductNotFound
+}
+
+func getNextID() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 func (p *Product) FromJSON(r io.Reader) error {
--- handlers/productss.go
+++ handlers/productss.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"main/work/data"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -35,9 +37,31 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.addProduct(rw, r)
 		return
 	}
-	//handle an update using restful based approach
+	//handle an update using restful based approach, expect ID in URI
 	if r.Method == http.MethodPut {
-		p.updateProduct(rw, r)
+		p.l.Println("PUT Request:")
+		r2 := regexp.MustCompile(`/([0-9]+)`)
+		g := r2.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			p.l.Println("Invalid URI more than one id")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		if len(g[0]) != 2 {
+			p.l.Println("Invalid URI more than one capture group")
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI unable to convert to number", idString)
+			http.Error(rw, "Invalid ID", http.StatusBadRequest)
+			return
+		}
+		p.l.Println("got id", id)
+
+		p.updateProduct(id, rw, r)
 		return
 	}
 	//catch rest
@@ -63,11 +87,35 @@ func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
 		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
 	}
 
-	p.l.Printf("Prod: %#v", prod)
+	if err == nil {
+		p.l.Printf("Prod: %#v", prod)
+		data.AddProduct(prod)
+	}
+
 }
 
-func (p *Products) updateProduct(rw http.ResponseWriter, r *http.Request) {
-	p.l.Println("Handle POST Request for: Products")
+func (p *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Request for: Products")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	if err == nil {
+		p.l.Printf("Prod: %#v", prod)
+		err = data.UpdateProduct(id, prod)
+		if err == data.ErrorProductNotFound {
+			http.Error(rw, "Product not found", http.StatusNotFound)
+			return
+		}
+
+		if err != nil {
+			http.Error(rw, "Product not found", http.StatusInternalServerError)
+			return
+		}
+	}
 }
 
 //Example Data for POST: curl -v localhost:9000 -d "{\"id\": 1, \"name\": \"tea\", \"description\": \"a nice cup of tea\"}"
